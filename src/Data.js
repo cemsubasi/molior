80e610df0b7eef350e19ed2e81ca3c015356b435
CemSubasi <cemsubasi@live.com>
2021-03-05 22:13:42 +0300

    .gitignore and heroku again

--- src/Data.js
+++ src/Data.js
@@ -13,8 +13,8 @@ export const homePageTitle = 'Munch Punch ve Negrodan Sevgilerle ';
 export const secret = 'munchhh';
 
 const instance = axios.create({
-		// baseURL: 'https://molior-server.herokuapp.com',
-		baseURL: 'http://127.0.0.1:4004',
+		baseURL: 'https://molior-server.herokuapp.com',
+		// baseURL: 'http://127.0.0.1:4004',
 		timeout: 50000,
 	headers: {
 				'Access-Control-Allow-Origin': '*',
